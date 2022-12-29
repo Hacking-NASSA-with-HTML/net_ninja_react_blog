7396b3b6619907adfdaf411315c0e6388e80ee37
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-29 21:42:40 +0400

    feat: add useState hook syntax

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
-const Home = () => {
+import { useState } from "react"
 
-    const handleClick = (e) => {
-        console.log('Hello, Ninjas', e)
-    }
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target)
+const Home = () => {
+    // let name = 'Mario'
+    const [name, setName] = useState('Mario')
+    const [age, setAge] = useState(25)
+
+    const handleClick = () => {
+        setName('Luigi')
+        setAge(30)
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age}</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('Jon', e)}>Click Me Again</button>
         </div>
     )
 }
