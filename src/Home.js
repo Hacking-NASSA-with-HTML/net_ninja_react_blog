f586f2d1a51b4daa2ba256e548aaf8db19a903ab
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-31 19:50:41 +0400

    feat: add Loader shown before data appears for rendering

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,28 @@ import BlogList from "./BlogList"
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    // Loader shown before data appear for rendering
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        // console.log('Use Effect ran')
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                // console.log(data)
-                setBlogs(data)
-            })
+        setTimeout(() => {
+            // console.log('Use Effect ran')
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    // console.log(data)
+                    setBlogs(data)
+                    setIsPending(false)
+                })
+        }, 4000);
     }, [])
 
     return (
         <div className="home">
+            {/* Loader shown before data appears for rendering */}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
     )
