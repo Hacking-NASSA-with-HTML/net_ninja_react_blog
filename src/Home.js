c97af9cc82007e674bacd1e26e51e4bc403de495
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-31 19:06:39 +0400

    feat: add new branch

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList"
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     const [name, setName] = useState('Mario')
 
@@ -17,14 +13,11 @@ const Home = () => {
 
     useEffect(() => {
         console.log('Use Effect ran')
-        console.log(name)
-    }, [name])
+    }, [])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
-            <button onClick={() => setName('Luigi')}>change name</button>
-            <p>{name}</p>
         </div>
     )
 }
